e0bc377425ab232df23eb1a3694cbf510757efec
calm1205 <naginani072@gmail.com>
2023-04-03 22:15:35 +0900

    get coffees

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+export const App = () => {
+  const [coffees, setCoffees] = useState(null);
+
+  const getData = async () => {
+    const response = await fetch("https://api.sampleapis.com/coffee/hot");
+    const json = await response.json();
+    setCoffees(json);
+  };
+
+  useEffect(() => getData, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Coffee List</h1>
+      {coffees?.map((coffee, index) => (
+        <div key={index}>{coffee?.title}</div>
+      ))}
+    </>
   );
-}
-
-export default App;
+};
