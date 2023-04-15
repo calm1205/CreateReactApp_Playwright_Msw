62e5bad78a8c564251ac1ef03ded121b90dfd2e5
Moriyama Nagi <naginani072@gmail.com>
2023-04-15 15:04:21 +0900

    Feature/sample playwright msw (#1)

--- src/__test__/index.spec.ts
+++ src/__test__/index.spec.ts
@@ -1,6 +1,7 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from "./customTest";
 
 test("visit localhost", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await expect(page.url()).toBe("http://localhost:3000/");
+  expect(page.url()).toBe("http://localhost:3000/");
+  await expect(page.getByText("mock coffee 1")).toBeVisible();
 });
